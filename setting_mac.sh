85e9d10e1c3e5f6116375cf36884b6ee7ebdb5ae
YasushiKobayashi <ptpadan@gmail.com>
2020-04-24 23:19:25 +0900

    update

--- setting_mac.sh
+++ setting_mac.sh
@@ -3,9 +3,8 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 
-brew install wget anyenv zsh-completions tmux tmuxinator zsh git neovim
+brew install wget anyenv zsh-completions tmux tmuxinator \
+  zsh git neovim helm peco bat evans ghq hub htop jq octant saml2aws tig tor yarn awscli direnv
 
 brew tap caskroom/cask
-brew cask install google-chrome docker iterm2
-
-wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh -O /usr/local/share/zsh/site-functions/_tmuxinator
+brew cask install google-chrome docker iterm2 1password firefox visual-studio-code sequel-pro
