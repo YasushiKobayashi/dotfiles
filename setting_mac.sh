b50223971a308e71d137f7921ece6b3360783732
YasushiKobayashi <ptpadan@gmail.com>
2023-05-09 19:31:58 +0900

    update cask

--- setting_mac.sh
+++ setting_mac.sh
@@ -6,5 +6,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install wget anyenv zsh-completions tmux tmuxinator \
   zsh git neovim helm peco bat evans ghq hub htop jq octant saml2aws tig tor yarn awscli direnv
 
+
 brew tap caskroom/cask
-brew cask install google-chrome docker iterm2 1password firefox visual-studio-code sequel-pro
+brew install --cask 1password/tap/1password-cli
+brew cask install google-chrome docker iterm2 1password 1password-cli firefox visual-studio-code sequel-pro sublime-text fork maccy appcleaner
